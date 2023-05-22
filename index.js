4485ef176c1eca086e71c1517585c4a68a0d0962
Athisham Ali <sahtisham735@gmail.com>
2023-05-22 20:50:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// Serve static files
+app.use(express.static('public', {
+  setHeaders: (res, path, stat) => {
+    if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
+
+// Store connected clients
+const clients = new Map();
+
+// Serve the HTML file with the form to input the client ID
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handle socket connection
+io.on('connection', (socket) => {
+  console.log('A client connected');
+
+  // Handle join message
+  socket.on('join-message', (clientId) => {
+    // Store the socket reference with the client ID
+    clients.set(clientId, socket);
+    console.log(`Client joined: ${clientId}`);
+  });
+  socket.on('screen-data', (screenshotData) => {
+    io.emit('screen-data', screenshotData);
+  });
+  // Handle disconnect
+  socket.on('disconnect', () => {
+    console.log('A client disconnected');
+
+    // Remove the socket reference from the stored clients
+    clients.forEach((value, key) => {
+      if (value === socket) {
+        clients.delete(key);
+        console.log(`Client left: ${key}`);
+      }
+    });
+  });
+});
+
+// Start the server
+const port = 5000;
+http.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
